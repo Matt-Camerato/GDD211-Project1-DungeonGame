5378468d486fff1aacc7fa9cf55c74cb3a9c6e2b
Matt-Camerato <matt.camerato@gmail.com>
2021-03-24 20:29:32 -0400

    Update 3
    Updated sprites and 2D lighting for base room prefabs

---
+++ Assets/Scripts/DungeonGeneration/LightFlicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField] private float minOuterRadius;
+    [SerializeField] private float maxOuterRadius;
+    [SerializeField] private float minIntensity;
+    [SerializeField] private float maxIntensity;
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+
+    private Light2D light2D;
+    private float randomSpeed;
+
+    private void Start()
+    {
+        light2D = GetComponent<Light2D>();
+        randomSpeed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    private void Update()
+    {
+        light2D.intensity = minIntensity + Mathf.PingPong(randomSpeed * Time.time, maxIntensity - minIntensity);
+        light2D.pointLightOuterRadius = minOuterRadius + Mathf.PingPong(randomSpeed/3 * Time.time, maxOuterRadius - minOuterRadius);
+        if(light2D.pointLightOuterRadius <= minOuterRadius)
+        {
+            randomSpeed = Random.Range(minSpeed, maxSpeed);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,16 +6,50 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
 
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Animator anim;
+
     private bool canMove = true; //<--this may be public in the future so other scripts can stop player movement
+
+    private void Start()
+    {
+        sr = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     private void FixedUpdate()
     {
+        rb.velocity = Vector2.zero; //reset velocity so player doesn't slide around
+
         if (canMove)
         {
-            float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-            float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+            float moveX = Input.GetAxis("Horizontal") * moveSpeed * 10 * Time.deltaTime;
+            float moveY = Input.GetAxis("Vertical") * moveSpeed * 10 * Time.deltaTime;
 
-            transform.position += new Vector3(moveX, moveY, 0);
+            if(new Vector2(moveX, moveY) != Vector2.zero)
+            {
+                //player is moving
+
+                anim.SetBool("walking", true);
+                rb.velocity = new Vector2(moveX, moveY);
+
+                if(moveX > 0)
+                {
+                    sr.flipX = true;
+                }
+                else if(moveX < 0)
+                {
+                    sr.flipX = false;
+                }
+            }
+            else
+            {
+                //player isn't moving
+
+                anim.SetBool("walking", false);
+            }
         }
     }
 }
